01d8b753f0e25b5eeba38fc5073d774ef8b3fdc0
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-11-25 15:56:35 -0600

    added in the questions to the questions array in index.js

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installInstructions',
+        message: 'Please provide the installation instructions for the project.'
+    },
+    {
+        type: 'input',
+        name: 'usageInfo',
+        message: 'What will this project be used for?'
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'For any users wishing to contribute to your project, are there any guidelines you would like them to follow?'
+    },
+    {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'Please provide the project test instructions.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Does this project have a license?'
+    },
+    {
+        type: 'input',
+        name: 'gitUsername',
+        message: 'Enter your GitHub username: '
+    },
+    {
+        type: 'input',
+        name: 'emailaddress',
+        message: 'Enter your GitHub password: '
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
