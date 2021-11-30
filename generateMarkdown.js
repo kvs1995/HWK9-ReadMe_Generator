a6ea4c8eb2b24a7a1c9af7ce26c82fe860da907d
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-11-30 00:47:54 -0600

    completion fo the readMe text and small updates to align variables

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -6,15 +6,15 @@ function renderLicenseBadge(license) {
   //use case switch 
   switch (license) {
     case 'MIT':
-     return '[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)]'
+     return `[![MIT License](<https://img.shields.io/badge/License-MIT-green.svg>)](https://opensource.org/licenses/MIT)`
     case 'GPLv3':
-      return '[![GPL v3 License](https://img.shields.io/badge/License-GPL%20v3-blue.svg)]'
+      return `[![GPL v3 License](<https://img.shields.io/badge/License-GPL%20v3-blue.svg>)](https://opensource.org/licenses/gpl-3.0)`
     case 'Apache 2.0':
-      return '[![Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-purple.svg)]'
+      return `[![Apache License 2.0](<https://img.shields.io/badge/License-Apache_2.0-purple.svg>)](https://opensource.org/licenses/apache-2.0)`
     case 'Mozilla Public License 2.0':
-      return '[![Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL_2.0-orange.svg)]'
+      return `[![Mozilla Public License 2.0](<https://img.shields.io/badge/License-MPL_2.0-orange.svg>)](https://opensource.org/licenses/MPL-2.0)`
     case 'No License':
-      return ''
+      return ``
   }
 }
 
@@ -25,18 +25,18 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
 //switch statement containing [license] (badge url)
 
-switch (license) {
-  case 'MIT':
-    return '[MIT License](https://opensource.org/licenses/MIT)'
-  case 'GPLv3':
-    return '[GPL v3 License](https://opensource.org/licenses/gpl-3.0)'
-  case 'Apache 2.0':
-    return '[Apache License 2.0](https://opensource.org/licenses/apache-2.0)'
-  case 'Mozilla Public License 2.0':
-    return '[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)'
-  case 'No License':
-    return ''
-  }
+  switch (license) {
+    case 'MIT':
+      return `[MIT License](https://opensource.org/licenses/MIT)`
+    case 'GPLv3':
+      return `[GPL v3 License](https://opensource.org/licenses/gpl-3.0)`
+    case 'Apache 2.0':
+      return `[Apache License 2.0](https://opensource.org/licenses/apache-2.0)`
+    case 'Mozilla Public License 2.0':
+      return `[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`
+    case 'No License':
+      return ``
+    }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -45,7 +45,7 @@ function renderLicenseSection(license) {
 //if the 'No License' is selected in the prompt, then no section should be created, but if there is then the license section should be added. 
   switch(license)  {
     case 'No License':
-      return '';
+      return ``;
     default:
       return `#License
               ##${license}
@@ -56,9 +56,9 @@ function renderLicenseSection(license) {
 function renderTableofContents(license){
   switch(license) {
     case 'No License':
-      return '';
+      return ``;
     default:
-      return '* [License](#license)' 
+      return `* [License](#license)` 
   }
 
 }
@@ -67,18 +67,43 @@ function renderTableofContents(license){
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
 
-          ${data.description} 
+  ${data.description} 
 
-          # Table of Contents
-          * [Installation](#installation)
-          * [Usage](#usage)
-          * [License](#license)
-          * [Contributing](#contributing)
-          * [Tests](#tests)
-          * [Questions](#questions)
-          ${renderTableofContents(data.license)}
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution Guidelines](#contribution-guidelines)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ${renderTableofContents(data.license)}
+
+  ---
+  ## Installations
+  ${data.installInstructions}
+
+  ## Usage
+  ${data.usageInfo}
+  
+  Take a look at this short video for a walkthrough on how to use the application:
+
+  ## Contribution Guidelines
+  ${data.contributionGuidelines}
+
+  ## Tests
+  ${data.testInstructions}
+
+  ## Questions
+  
+  If you have any questions or would like to contact me regarding aspects of this project, I can be reached at:
+  * GitHub: ${data.gitUsername}
+  * Email: ${data.emailAddress}
+
+
+  ## License
+  ${data.license}
 `;
 }
 
